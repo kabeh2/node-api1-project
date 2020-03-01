02e88e0662f8a57eaa201f3c099f338d6edb2204
kabeh2 <mattcnk@hotmail.com>
2020-02-29 19:24:30 -0500

    Added page transitions with react-spring

--- client/src/components/Nav.jsx
+++ client/src/components/Nav.jsx
@@ -1,24 +1,19 @@
 import React, { useState } from "react";
-import { useLocation, useRouteMatch } from "react-router";
+import { useLocation } from "react-router";
 import { Menu } from "antd";
 import { TeamOutlined, UserAddOutlined } from "@ant-design/icons";
 import { Link } from "react-router-dom";
 
 const Nav = () => {
   const locationUrl = useLocation().pathname.match(/([a-z])+/gim);
-  const match = useRouteMatch();
 
   const initialState = {
     current: locationUrl === null ? "home" : locationUrl[0]
   };
 
-  console.log("LOCATION: ", locationUrl);
-  console.log("MATCH: ", match);
-
   const [state, setState] = useState(initialState);
 
   const handleClick = e => {
-    console.log("click ", e);
     setState({
       current: e.key
     });
