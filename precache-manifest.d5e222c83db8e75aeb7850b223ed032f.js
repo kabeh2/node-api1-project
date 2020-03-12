5b859d79418c7341264b2d3d323a6df0d76d0f59
kabeh2 <mattcnk@hotmail.com>
2020-03-12 13:46:18 -0400

    Updates

---
+++ precache-manifest.d5e222c83db8e75aeb7850b223ed032f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2d5f44af54628090b14b1eda8be4436b",
+    "url": "/node-api1-project/index.html"
+  },
+  {
+    "revision": "a5d1f4a3759e558e70d8",
+    "url": "/node-api1-project/static/css/2.03d7f783.chunk.css"
+  },
+  {
+    "revision": "499ba7432e67ee73dae0",
+    "url": "/node-api1-project/static/css/main.69d7c72a.chunk.css"
+  },
+  {
+    "revision": "a5d1f4a3759e558e70d8",
+    "url": "/node-api1-project/static/js/2.efd83dc1.chunk.js"
+  },
+  {
+    "revision": "fbe8bb9193678c10201555f69f177594",
+    "url": "/node-api1-project/static/js/2.efd83dc1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "499ba7432e67ee73dae0",
+    "url": "/node-api1-project/static/js/main.f8d60c15.chunk.js"
+  },
+  {
+    "revision": "2235598c89e402874f6e",
+    "url": "/node-api1-project/static/js/runtime-main.67cd0639.js"
+  }
+]);
